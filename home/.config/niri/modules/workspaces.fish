11824b33ca2276123898789b72e4116c5ae8f115
Ruben Garcia <arqueonautis@gmail.com>
2024-12-05 19:49:21 -0600

    c

--- home/.config/niri/modules/workspaces.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-while true
-    sleep 0.2s
-
-    niri msg --json workspaces \
-        | jq -r ".[] | if .is_active then \"󰪥\" else \"󰄰\" end" \
-        | string join " "
-end
